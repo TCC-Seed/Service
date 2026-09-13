f807f8093d587986fc194536417a2b44a9e8fe6c
João Antonio Ginuino Carvalho <joaoseisei@gmail.com>
2026-09-13 12:41:04 -0300

    feat: adiciona aplicação spring

---
+++ src/main/java/com/tccseed/tcc_seed/TccSeedApplication.java
@@ -0,0 +1,13 @@
+package com.tccseed.tcc_seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccSeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tccseed/tcc_seed/TccSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tccseed.tcc_seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccSeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
